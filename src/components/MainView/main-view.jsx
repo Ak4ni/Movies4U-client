af8d79361163e6ed650392067d200ed2512d85d2
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-01-21 08:46:21 -0500

    Update main-view.jsx

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -6,9 +6,9 @@ export class MainView extends React.Component{
         super();
         this.state = {
           movies: [
-            { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...'},
-            { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-            { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
+            { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream sharing technology is given the inverse task of planting an idea into the mind of a C.E.O. but his tragic past may doom the project and his team to disaster.', ImagePath:'https://images-na.ssl-images-amazon.com/images/S/apesafeframe/ape/sf/desktop/DAsf-1.50.b9438f1.js'},
+            { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://images-na.ssl-images-amazon.com/images/S/apesafeframe/ape/sf/desktop/DAsf-1.50.b9438f1.js'},
+            { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery', ImagePath: '"https://images-na.ssl-images-amazon.com/images/S/apesafeframe/ape/sf/desktop/DAsf-1.50.b9438f1.js"'}
           ],
           selectedMovies:null
         };
@@ -17,18 +17,8 @@ export class MainView extends React.Component{
         this.setState({
           selectedMovie: newSelectedMovie
         });
-      }
-      render() {
-        return(
-            <div className="main-view">
-                <div>Inceprtion</div>
-                <div>The Shawsshank Redemption</div>
-                <div>Gladiator</div>
-            </div>
-        );
     }
- 
-   render() {
+     render() {
     const { movies, selectedMovie } = this.state;
     if (movies.length === 0) 
     if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
