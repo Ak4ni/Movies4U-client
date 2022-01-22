cf547183a5533651bc47375f525d2d4f2e4b423f
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-01-22 14:12:35 -0500

    Update login-view.jsx

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -1,44 +1,51 @@
-import React,{useState} from "react";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './login-view.scss';
+import { Form, Button, Container, Row, Col, Card, CardGroup } from 'react-bootstrap';
 
 export function LoginView(props) {
     const [ username, setUsername ] = useState('');
-    const [ password, setPassword ]= useState('');
-    
-    
+    const [ password, setPassword ] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(username, password);
-    }
-      return(
-        <Container>
-        <Row>
-            <Col>
-                <CardGroup>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Please login</Card.Title>
-                            <Form>
-                                <Form.Group controlId="formUsername">
-                                    <Form.Label>Username:</Form.Label>
-                                    <Form.Control type="text" onChange={e => setUsername(e.target.value)} placeholder="Enter username" />
-                                </Form.Group>
+        /* Send a request to the server for authentication */
+        /* then call props.onLoggedIn(username) */
+        props.onLoggedIn(username);
+    };
 
-                                <Form.Group controlId="formPassword">
-                                    <Form.Label>Password:</Form.Label>
-                                    <Form.Control type="password" onChange={e => setPassword(e.target.value)} placeholder="Enter password" />
-                                </Form.Group>
-                                <Button variant="outline-light" type="submit" onClick={handleSubmit}>
-                                    Submit
-                                </Button>
-                            </Form>
-                        </Card.Body>
-                    </Card>
-                </CardGroup>
-            </Col>
-        </Row>
-    </Container>
-);
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Please login</Card.Title>
+                                <Form>
+                                    <Form.Group controlId="formUsername">
+                                        <Form.Label>Username:</Form.Label>
+                                        <Form.Control type="text" onChange={e => setUsername(e.target.value)} placeholder="Enter username" />
+                                    </Form.Group>
+
+                                    <Form.Group controlId="formPassword">
+                                        <Form.Label>Password:</Form.Label>
+                                        <Form.Control type="password" onChange={e => setPassword(e.target.value)} placeholder="Enter password" />
+                                    </Form.Group>
+                                    <Button variant="outline-light" type="submit" onClick={handleSubmit}>
+                                        Submit
+                                    </Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
+
 LoginView.propTypes = {
     user: PropTypes.shape({
         username: PropTypes.string.isRequired,
@@ -46,4 +53,3 @@ LoginView.propTypes = {
     }),
     onLoggedIn: PropTypes.func.isRequired,
 };
-
