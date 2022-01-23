2effd3c6451e54f7da2302e1ca40e125d9133c3a
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-01-23 16:51:58 -0500

    Update login-view.jsx

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -11,8 +11,9 @@ export function LoginView(props) {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(username, password);
-         // Send a request to the server for authentication, then call props.onLoggedIn(username)
-         props.onLoggedIn(username)
+         // Send a request to the server for authentication, 
+         //then call props.onLoggedIn(username)
+        props.onLoggedIn(username)
     };
       
     return(
