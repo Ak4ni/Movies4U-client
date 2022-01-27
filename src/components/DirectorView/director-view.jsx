cbe5e543b5a6fc0240750a81507157d4af7561b4
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-01-27 18:03:16 -0500

    Create director-view.jsx

---
+++ src/components/DirectorView/director-view.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Card, Button } from "react-bootstrap";
+
+import "./director-view.scss";
+
+export class DirectorView extends React.Component {
+  render() {
+    const { Director } = this.props;
+
+    return (
+      <Container>
+        <br />
+        <Card bg="secondary" text="light" border="light" align="center">
+          <Card.Body>
+            <Card.Title>Director</Card.Title>
+            <div>
+              <span className="label">Name:</span>
+              <span className="value">{Director.Name}</span>
+            </div>
+            <div>
+              <span className="label">Bio:</span>
+              <span className="value">{Director.Bio}</span>
+            </div>
+            <div>
+              <span className="label">Born:</span>
+              <span className="value">{Director.Birthyear}</span>
+            </div>
+            <div>
+              <span className="label">Death:</span>
+              <span className="value">{Director.Deathyear}</span>
+            </div>
+            <br />
+            <div className="backButton">
+              <Link to={`/`}>
+                <Button className="returnButton" variant="dark">
+                  Back
+                </Button>
+              </Link>
+            </div>
+          </Card.Body>
+        </Card>
+      </Container>
+    );
+  }
+}
