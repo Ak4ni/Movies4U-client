f2521152c7a25c2e35c36a8485a6bf593bea4944
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-01-27 18:02:59 -0500

    Update movie-view.jsx

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './movie-view.scss';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import {Container, Row, Col, Button} from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -17,7 +17,7 @@ export class MovieView extends React.Component {
     }
 
     render() {
-        const { movie, onBackClick } = this.props;
+        const {movie, onBackClick} = this.props;
 
         return (
             <Container fluid className="moviesContainer">
@@ -25,26 +25,47 @@ export class MovieView extends React.Component {
                     <Col>
                         <div className="movie-view">
                             <div className="movie-poster">
-                                <img src={movie.ImagePath}/>
+                                <img src={
+                                    movie.ImagePath
+                                }/>
                             </div>
                             <div className="movie-title">
-                                <span className="label">Title: </span>
-                                <span className="value">{movie.Title}</span>
+                                <span className="label">Title:
+                                </span>
+                                <span className="value">
+                                    {
+                                    movie.Title
+                                }</span>
                             </div>
                             <div className="movie-description">
-                                <span className="label">Description: </span>
-                                <span className="value">{movie.Description}</span>
+                                <span className="label">Description:
+                                </span>
+                                <span className="value">
+                                    {
+                                    movie.Description
+                                }</span>
                             </div>
                             <div className="movie-genre">
-                                <span className="label">Genre: </span>
-                                <span className="value">{movie.Genre.Name}</span>
+                                <span className="label">Genre:
+                                </span>
+                                <span className="value">
+                                    {
+                                    movie.Genre.Name
+                                }</span>
                             </div>
                             <div className="movie-director">
-                                <span className="label">Director: </span>
-                                <span className="value">{movie.Director.Name}</span>
+                                <span className="label">Director:
+                                </span>
+                                <span className="value">
+                                    {
+                                    movie.Director.Name
+                                }</span>
                             </div>
 
-                            <Button variant="outline-light" onClick={() => onBackClick(null)}>Back</Button>
+                            <Button variant="outline-light"
+                                onClick={
+                                    () => onBackClick(null)
+                            }>Back</Button>
 
                         </div>
                     </Col>
