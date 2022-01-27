caf27437d4adf1d02b48d3964eb2330a5b440cd5
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-01-27 18:03:01 -0500

    Update movie-card.jsx

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,33 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './movie-card.scss';
-import { Card, Button } from 'react-bootstrap';
+import React from "react";
+import PropTypes from "prop-types";
+import "./movie-card.scss";
+import {Card, Button} from "react-bootstrap";
 
+import {Link} from "react-router-dom";
 
 export class MovieCard extends React.Component {
-  render() {
-    const { movie, onMovieClick } = this.props;
+    render() {
+        const {movie} = this.props;
 
-    return (
-     <Card>
-         <Card.Img variant="top" src={movie.ImagePath} />
-         <Card.Body>
-             <Card.Title>{movie.Title}</Card.Title>
-             <Card.Text>{movie.Description}</Card.Text>
-             <Button onClick={() => onMovieClick(movie)} variant='link'>Open</Button>
-         </Card.Body>
-     </Card>
-    );
-  }
-}
-
-MovieCard.propTypes = {
-    movie: PropTypes.shape({
-        Title: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired,
-        ImagePath: PropTypes.string.isRequired,
-        Genre: PropTypes.string.isRequired,
-        Director: PropTypes.string.isRequired
-    }).isRequired,
+        return (
+            <Card>
+                <Card.Img variant="top"
+                    src={
+                        movie.ImagePath
+                    }/>
+                <Card.Body>
+                    <Card.Title>{
+                        movie.Title
+                    }</Card.Title>
+                    <Card.Text>{
+                        movie.Description
+                    }</Card.Text>
+                    <Link to={
+                        `/movies/${
+                            movie._id
+                        }`
+                    }>
+                        <Button variant="link">Open</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+        );
+    }
+} MovieCard.propTypes = {
+    movie: PropTypes.shape(
+        {
+            Title: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired,
+            ImagePath: PropTypes.string.isRequired,
+            Genre: PropTypes.string.isRequired,
+            Director: PropTypes.string.isRequired
+        }
+    ).isRequired,
     onMovieClick: PropTypes.func.isRequired
 };
