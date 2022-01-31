fa5e219692e6add7cf4e2e2618ade66a5b7f7b31
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-01-31 12:07:53 -0500

    Update movie-view.jsx

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -76,22 +76,3 @@ export class MovieView extends React.Component {
     }
 }
 
-MovieView.propTypes = {
-    movie: PropTypes.shape({
-      Title: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired,
-      Genre: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired
-      }),
-      Director: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Bio: PropTypes.string.isRequired,
-        Birthyear: PropTypes.string,
-        Deathyear: PropTypes.string
-      }),
-      Featured: PropTypes.bool,
-      ImagePath: PropTypes.string.isRequired
-      }).isRequired,
-      onBackClick: PropTypes.func.isRequired
-  };
