5715ebb1ee0107305d40740caa284343c04f5785
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-02-04 10:18:10 -0500

    Update movie-view.jsx

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 import './movie-view.scss';
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
 
@@ -17,7 +20,7 @@ export class MovieView extends React.Component {
     }
 
     render() {
-        const {movie, onBackClick} = this.props;
+        const { movie, onBackClick } = this.props;
 
         return (
             <Container fluid className="moviesContainer">
@@ -25,47 +28,30 @@ export class MovieView extends React.Component {
                     <Col>
                         <div className="movie-view">
                             <div className="movie-poster">
-                                <img src={
-                                    movie.ImagePath
-                                }/>
+                                <img src={movie.ImagePath}/>
                             </div>
                             <div className="movie-title">
-                                <span className="label">Title:
-                                </span>
-                                <span className="value">
-                                    {
-                                    movie.Title
-                                }</span>
+                                <span className="label">Title: </span>
+                                <span className="value">{movie.Title}</span>
                             </div>
                             <div className="movie-description">
-                                <span className="label">Description:
-                                </span>
-                                <span className="value">
-                                    {
-                                    movie.Description
-                                }</span>
+                                <span className="label">Description: </span>
+                                <span className="value">{movie.Description}</span>
                             </div>
                             <div className="movie-genre">
-                                <span className="label">Genre:
-                                </span>
-                                <span className="value">
-                                    {
-                                    movie.Genre.Name
-                                }</span>
+                                <span className="label">Genre: </span>
+                                <Link to={`/genres/${movie.Genre.Name}`}>
+                                    <Button variant="link">{movie.Genre.Name}</Button>
+                                </Link>
                             </div>
                             <div className="movie-director">
-                                <span className="label">Director:
-                                </span>
-                                <span className="value">
-                                    {
-                                    movie.Director.Name
-                                }</span>
+                                <span className="label">Director: </span>
+                                <Link to={`/directors/${movie.Director.Name}`}>
+                                    <Button variant="link">{movie.Director.Name}</Button>
+                                </Link>
                             </div>
 
-                            <Button variant="outline-light"
-                                onClick={
-                                    () => onBackClick(null)
-                            }>Back</Button>
+                            <Button variant="outline-light" onClick={() => onBackClick(null)}>Back</Button>
 
                         </div>
                     </Col>
@@ -74,4 +60,3 @@ export class MovieView extends React.Component {
         );
     }
 }
-
