0fd2a51b41b8f6d059c7689aa9c76b92f9bd8300
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-02-04 10:18:18 -0500

    Update login-view.jsx

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -1,119 +1,102 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import axios from "axios";
-import {
-  Form,
-  Button,
-  Card,
-  CardGroup,
-  Col,
-  Row,
-  Container,
-} from "react-bootstrap";
-import { Link } from "react-router-dom";
-
-import "./login-view.scss";
+import React, { useState } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import './login-view.scss';
+import { Link } from 'react-router-dom';
+import { Form, Button, Container, Row, Col, Card, CardGroup } from 'react-bootstrap';
 
 export function LoginView(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  // declare hook for each input
-  const [usernameErr, setUsernameErr] = useState("");
-  const [passwordErr, setPaswordErr] = useState("");
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
 
-  // validate user inputs
-  const validate = () => {
-    let isReq = true;
-    if (!username) {
-      setUsernameErr("Username Required");
-      isReq = false;
-    } else if (username.length < 6) {
-      setUsernameErr("Username must be 6 characters long");
-      isReq = false;
-    }
-    if (!password) {
-      setPaswordErr("Password Required");
-      isReq = false;
-    } else if (password.length < 8) {
-      setPassword("Password must be at least 8 characters long");
-      isReq = false;
-    }
-    return isReq;
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const isReq = validate();
-    if (isReq) {
-      /* Send a request to the server for authentication */
-      axios
-        .post("https://themovies4u.herokuapp.com/login", {
-          Username: username,
-          Password: password,
-        })
-        .then((response) => {
-          const data = response.data;
-          props.onLoggedIn(data);
-        })
-        .catch((e) => {
-          console.log("no such user");
-        });
-    }
-  };
+    // Declare hook for each input
+    const [ usernameErr, setUsernameErr ] = useState('');
+    const [ passwordErr, setPasswordErr ] = useState('');
 
-  return (
-    <div className="login-view">
-      <Container>
-        <Row>
-          <Col>
-            <CardGroup>
-              <Card>
-                <Card.Body>
-                  <Card.Title>Welcome to Movies4U!</Card.Title>
-                  <Form>
-                    <Form.Group controlId="formUsername">
-                      <Form.Label>Username:</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Enter username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                      />{" "}
-                      {usernameErr && <p>{usernameErr}</p>}{" "}
-                    </Form.Group>
+    // validate user inputs
+    const validate = () => {
+        let isReq = true;
 
-                    <Form.Group controlId="formPassword">
-                      <Form.Label>Password:</Form.Label>
-                      <Form.Control
-                        type="password"
-                        placeholder="Enter password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                      />{" "}
-                      {passwordErr && <p>{passwordErr}</p>}{" "}
-                    </Form.Group>
-                    <Button
-                      variant="primary"
-                      type="submit"
-                      onClick={handleSubmit}
-                    >Submit
-                    </Button>
-                    <Link to={`/register`} className="float-right">
-                      <Button type="button">Register</Button>
-                    </Link>
-                  </Form>
-                </Card.Body>
-              </Card>
-            </CardGroup>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+        if(!username){
+            setUsernameErr('Username required');
+            isReq = false;
+        }else if(username.length < 2){
+            setUsernameErr('Username must be at least 2 characters long');
+            isReq = false;
+        }
+        if(!password){
+            setPasswordErr('Password required');
+            isReq = false;
+        }else if(password.length < 6){
+            setPassword('Password must be at least 6 characters long');
+            isReq = false;
+        }
+
+        return isReq;
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isReq = validate();
+        if(isReq) {
+            /* Send request to the server for authentication */
+            axios.post(`https://themovies4u.herokuapp.com/login`, {
+                Username: username,
+                Password: password
+            })
+                .then(response =>{
+                    const data = response.data;
+                    props.onLoggedIn(data);
+                })
+                .catch(error => {
+                    console.log(error, 'no such user');
+                });
+        }
+    };
+
+    return (
+        <Container className="profile-view" align="center">
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Please login</Card.Title>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formUsername">
+                                        <Form.Label>Username:</Form.Label>
+                                        <Form.Control type="text" onChange={e => setUsername(e.target.value)} placeholder="Enter username" />
+                                        {/* code added here to display validation error */}
+                                        {usernameErr && <p>{usernameErr}</p>}
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3" controlId="formPassword">
+                                        <Form.Label>Password:</Form.Label>
+                                        <Form.Control type="password" onChange={e => setPassword(e.target.value)} placeholder="Enter password" />
+                                        {/* code added here to display validation error */}
+                                        {passwordErr && <p>{passwordErr}</p>}
+                                    </Form.Group>
+                                    <div className="mt-3">
+                                        <Button variant="success" type="submit" onClick={handleSubmit}>Submit</Button>
+                                        <Link to="/register">
+                                            <Button className="ml-3" variant="secondary">Register now</Button>
+                                        </Link>
+                                    </div>
+
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 LoginView.propTypes = {
-  user: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired,
-  }),
+    user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired
+    }),
+    onLoggedIn: PropTypes.func.isRequired,
 };
