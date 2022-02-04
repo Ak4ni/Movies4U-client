05716d18dbc9dda8b2abe70841d6626007ac9d70
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-02-04 10:21:52 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import axios from 'axios';
-import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom';
-import { MainView } from './components/MainView/main-view';
-import Container from 'react-bootstrap/Container';
-
+import React from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
+import { MainView } from "./components/MainView/main-view";
+import Container from "react-bootstrap/Container";
 
 // Import statement to indicate that you need to bundle `./index.scss`
-import './index.scss';
+import "./index.scss";
 
 // Main component (will eventually use all the others)
 class Movies4UApplication extends React.Component {
-    render() {
-        return (
-            <MainView />
-        );
-    }
+  render() {
+    return <MainView />;
+  }
 }
 
 // Find the root of myFlix app
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName("app-container")[0];
 
 // Tell React to render myFlix app in the root DOM element
 ReactDOM.render(<Movies4UApplication />, container);
