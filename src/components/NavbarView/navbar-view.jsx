363ce1b83947618dfb2c7784c9b1e3cd856f93be
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-02-04 10:18:07 -0500

    Update navbar-view.jsx

--- src/components/NavbarView/navbar-view.jsx
+++ src/components/NavbarView/navbar-view.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import {Container, Navbar, Nav, Button} from 'react-bootstrap';
-import './navbar-view.scss'
+import React from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+
+import './navbar-view.scss';
 
 export function NavbarView({user}) {
     const onLoggedOut = () => {
         localStorage.clear();
-        window.open("/", "_self");
-    }
+        window.open('/', '_self');
+    };
+
     const isAuth = () => {
         if (typeof window == "undefined") {
             return false;
@@ -17,43 +19,29 @@ export function NavbarView({user}) {
             return false;
         }
     };
+
     return (
-
-
-        <Navbar className="main-nav" sticky="top" bg="dark" expand="lg" variant="dark">
-            <Container>
+        <Navbar className="main-nav" sticky="top" expand="lg" variant="dark">
+            <Container fluid>
                 <Navbar.Brand className="navbar-logo" href="/">Movies4U</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="resposinve-navbar-nav">
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ml-auto">
-                        {
-                        isAuth() && (
-                            <Nav.Link href={
-                                `/users/${user}`
-                            }>
-                                {user}</Nav.Link>
-                        )
-                    }
-                        {
-                        isAuth() && (
-                            <Button variant="link"
-                                onClick={
-                                    () => {
-                                        this.onLoggedOut()
-                                    }
-                            }>Logout</Button>
-                        )
-                    }
-                        {
-                        ! isAuth() && (
-                            <Nav.Link href="/">Sign in</Nav.Link>
-                        )
-                    }
-                        {
-                        ! isAuth() && (
-                            <Nav.Link href="/register">Sign up</Nav.Link>
-                        )
-                    } </Nav>
+                        {isAuth() && (
+                            <Nav.Link href="/profile">{user}</Nav.Link>
+                        )}
+                        {isAuth() && (
+                            <Button variant="link" onClick={() => {
+                                onLoggedOut()
+                            }}>Logout</Button>
+                        )}
+                        {!isAuth() && (
+                            <Nav.Link href="/">Login</Nav.Link>
+                        )}
+                        {!isAuth() && (
+                            <Nav.Link href="/register">Register</Nav.Link>
+                        )}
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
