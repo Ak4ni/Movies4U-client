729b5f8463c3e433a5ca3144570c9fecb9c8166e
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-02-05 15:21:14 -0500

    Update navbar-view.jsx

--- src/components/NavbarView/navbar-view.jsx
+++ src/components/NavbarView/navbar-view.jsx
@@ -1,61 +1,50 @@
 import React from "react";
-import {Container, Navbar, Nav, Button} from 'react-bootstrap';
-import './navbar-view.scss'
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 
-export function NavbarView({user}) {
-    const onLoggedOut = () => {
-        localStorage.clear();
-        window.open("/", "_self");
+import "./navbar-view.scss";
+
+export function NavbarView({ user }) {
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open("/", "_self");
+  };
+
+  const isAuth = () => {
+    if (typeof window == "undefined") {
+      return false;
     }
-    const isAuth = () => {
-        if (typeof window == "undefined") {
-            return false;
-        }
-        if (localStorage.getItem("token")) {
-            return localStorage.getItem("token");
-        } else {
-            return false;
-        }
-    };
-    return (
+    if (localStorage.getItem("token")) {
+      return localStorage.getItem("token");
+    } else {
+      return false;
+    }
+  };
 
-
-        <Navbar className="main-nav" sticky="top" bg="dark" expand="lg" variant="dark">
-            <Container>
-                <Navbar.Brand className="navbar-logo" href="/">Movies4U</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="resposinve-navbar-nav">
-                    <Nav className="ml-auto">
-                        {
-                        isAuth() && (
-                            <Nav.Link href={
-                                `/users/${user}`
-                            }>
-                                {user}</Nav.Link>
-                        )
-                    }
-                        {
-                        isAuth() && (
-                            <Button variant="link"
-                                onClick={
-                                    () => {
-                                        this.onLoggedOut()
-                                    }
-                            }>Logout</Button>
-                        )
-                    }
-                        {
-                        ! isAuth() && (
-                            <Nav.Link href="/">Sign in</Nav.Link>
-                        )
-                    }
-                        {
-                        ! isAuth() && (
-                            <Nav.Link href="/register">Sign up</Nav.Link>
-                        )
-                    } </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar className="main-nav" sticky="top" expand="lg" variant="dark">
+      <Container fluid>
+        <Navbar.Brand className="navbar-logo" href="/">
+          Movies4U
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            {isAuth() && <Nav.Link href="/profile">{user}</Nav.Link>}
+            {isAuth() && (
+              <Button
+                variant="link"
+                onClick={() => {
+                  onLoggedOut();
+                }}
+              >
+                Logout
+              </Button>
+            )}
+            {!isAuth() && <Nav.Link href="/">Login</Nav.Link>}
+            {!isAuth() && <Nav.Link href="/register">Register</Nav.Link>}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
