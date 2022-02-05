f385bbad98a464bce38c3e97fd6156ddf8c37d8a
Andres Rivera Perez <89215774+Ak4ni@users.noreply.github.com>
2022-02-05 15:21:24 -0500

    Update genre-view.jsx

--- src/components/GenreView/genre-view.jsx
+++ src/components/GenreView/genre-view.jsx
@@ -1,34 +1,60 @@
 import React from "react";
-import PropTypes from 'prop-types'
-import { Link } from "react-router-dom";
-import { Button, Card } from "react-bootstrap";
-
+import axios from "axios";
+import PropTypes from "prop-types";
 import "./genre-view.scss";
+import { Link } from "react-router-dom";
+import { Container, Card, Button, Row } from "react-bootstrap";
 
 export class GenreView extends React.Component {
   render() {
-    const { Genre } = this.props;
+    const { genre, onBackClick, movies } = this.props;
+
     return (
-      <>
-        <Card bg="secondary" text="light" border="light" align="center">
+      <Container fluid>
+        <Card>
           <Card.Body>
             <Card.Title>Genre</Card.Title>
-            <div className="genre-name">
-              <span className="label">Name:</span>
-              <span className="value">{Genre.Name}</span>
-            </div>
-            <div className="genre-description">
-              <span className="label">Description:</span>
-              <span className="value">{Genre.Description}</span>
-            </div>
-            <Link to={`/`}>
-              <Button className="returnButton" variant="">
-                Return
-              </Button>
-            </Link>
+            <Card.Text>
+              <span className="label">Name: </span>
+              <span className="value">{genre.Name}</span>
+            </Card.Text>
+            <Card.Text>
+              <span className="label">Description: </span>
+              <span className="value">{genre.Description}</span>
+            </Card.Text>
+
+            <Button
+              variant="outline-light"
+              onClick={() => {
+                onBackClick();
+              }}
+            >
+              Back
+            </Button>
           </Card.Body>
         </Card>
-      </>
+        <Row>
+          {movies.map((movie) => (
+            <Card className="favorite-movie card-content" key={movie._id}>
+              <Card.Img
+                className="fav-poster"
+                variant="top"
+                src={movie.ImagePath}
+              />
+              <Card.Body style={{ backgroundColor: "black" }}>
+                <Card.Title className="movie_title">{movie.Title}</Card.Title>
+              </Card.Body>
+            </Card>
+          ))}
+        </Row>
+      </Container>
     );
   }
 }
+
+GenreView.proptypes = {
+  genre: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+  }).isRequired,
+};
