a30285fd0de6bb829393b52d8702a32eee82c898
Andres Rivera <89215774+Ak4ni@users.noreply.github.com>
2022-02-16 16:37:32 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension'
+
+
 import { MainView } from "./components/MainView/main-view";
+
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
+
+const store = createStore(moviesApp, devToolsEnhancer);
+
 // Main component (will eventually use all the others)
 class Movies4UApplication extends React.Component {
   render() {
-    return <MainView />;
-  }
-}
+    return(
+      <Provider store={store}>
+         <Container>
+           <MainView />
+         </Container>
+      </Provider>
+     );
+   }
+ }
 
 // Find the root of Movies4U app
 const container = document.getElementsByClassName("app-container")[0];
