520492bde28c4a9919c0d60960a7d017720ff28b
Andres Rivera <89215774+Ak4ni@users.noreply.github.com>
2022-02-22 09:54:55 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import moviesApp from './reducers/reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import Container from "react-bootstrap/Container";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import moviesApp from "./reducers/reducers";
 
-
-import  MainView  from "./components/MainView/main-view";
-
+import MainView from "./components/MainView/main-view";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
-
-const movies4UStore = createStore(moviesApp, window.REDEX_DEVTOOLS_EXTENSION && window.REDEX_DEVTOOLS_EXTENSION());
+const movies4UStore = createStore(
+  moviesApp,
+  window.REDEX_DEVTOOLS_EXTENSION && window.REDEX_DEVTOOLS_EXTENSION()
+);
 
 // Main component (will eventually use all the others)
 class Movies4UApplication extends React.Component {
   render() {
-    return(
+    return (
       <Provider store={movies4UStore}>
-         <Container>
-           <MainView />
-         </Container>
+        <Container>
+          <MainView />
+        </Container>
       </Provider>
-     );
-   }
- }
+    );
+  }
+}
 
 // Find the root of Movies4U app
 const container = document.getElementsByClassName("app-container")[0];
 
 // Tell React to render Movies4U app in the root DOM element
 ReactDOM.render(React.createElement(Movies4UApplication), container);
+
+const cors = require("cors");
+let allowedOrigins = ["https://themovies4u.netlify.app/"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
